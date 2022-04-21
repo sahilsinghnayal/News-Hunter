54e5b4ac64ba62754ab7703f4a59e5f25d554d6b
sahilsinghnayal <sahilsinghnayal@gmail.com>
2022-04-21 16:24:14 +0530

    btn

--- src/components/Newsitem.js
+++ src/components/Newsitem.js
@@ -10,10 +10,10 @@ export class Newsitem extends Component {
 
       <div>
         <div className="card" style={{width: "18rem"}}>
-  <img src={imageurl} className="card-img-top" alt="..."/>
+  <img src={!imageurl?"https://images.news18.com/ibnlive/uploads/2021/12/delhi-crowded-market-covid-164092567416x9.jpg":imageurl} className="card-img-top" alt=""/>
   <div className="card-body">
     <h5 className="card-title">{title}..</h5>
-    <p className="card-text">{description}..</p>
+    <p className="card-text">{description}</p>
     <a href={newsurl} target="_blank" rel="noreferrer" className="btn btn-primary">Read more</a>
   </div>
 </div>
